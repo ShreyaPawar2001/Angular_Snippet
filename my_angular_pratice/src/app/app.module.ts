aba2268d0b6cd6370801d840fadfd0d82c22daaf
ShreyaPawar2001 <shreyapawar2001@gmail.com>
2023-06-10 16:17:14 +0530

    Forms

--- my_angular_pratice/src/app/app.module.ts
+++ my_angular_pratice/src/app/app.module.ts
@@ -3,20 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // import { AppRoutingModule } from './app.component';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { LandingComponent } from './landing/landing.component';
+
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignUpComponent,
-    LandingComponent
+    
   ],
   imports: [
     BrowserModule,
+    
     FormsModule
   ],
   providers: [],
