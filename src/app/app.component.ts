54ba539f37cf1e64aa6b28162e972f4401c76558
ShreyaPawar2001 <shreyapawar2001@gmail.com>
2023-09-18 21:37:02 +0530

    TaskCompleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pratice';
+  circles: { id: number, isSelected: boolean, backgroundColor: string }[] = [];
+  nextCircleId = 1;
+  selectedCircle?: { id: number, isSelected: boolean, backgroundColor: string } ;
+
+
+  toggleSelectCircle(circle: { id: number, isSelected: boolean, backgroundColor: string }) {
+    console.log(circle);
+    console.log(this.selectedCircle);
+    
+     if (circle === this.selectedCircle) {
+       circle.isSelected = !circle.isSelected;
+       circle.backgroundColor == 'blue'? circle.backgroundColor ='grey' :circle.backgroundColor ='blue'
+     } else {
+       if(this.selectedCircle) {
+         this.selectedCircle.isSelected = false;
+         this.selectedCircle.backgroundColor = 'blue'
+       }
+       circle.isSelected = true;
+       circle.backgroundColor = 'grey'
+       this.selectedCircle = circle;
+     }
+   }
+   addCircle() {
+    const newCircle = {
+      id: this.nextCircleId,
+      isSelected: false,
+      //backgroundColor: this.nextCircleId % 2 === 0 ? 'blue' : 'red'
+      backgroundColor:'blue'
+    };
+    this.circles.push(newCircle);
+    this.nextCircleId++;
+  }
+  
 }
